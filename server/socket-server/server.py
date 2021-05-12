4efa7f8fdb7fac07693cca44d95ac952d5e9371e
dangost <dangost16@gmail.com>
2021-05-12 03:04:33 +0300

    asdf

--- server/socket-server/server.py
+++ server/socket-server/server.py
@@ -131,5 +131,5 @@ class Server:
             return None
 
 
-server = Server("192.168.100.5", 9090)
-# server = Server("77.223.97.149", 9094)
+# server = Server("192.168.100.5", 9090)
+server = Server("77.223.97.149", 9090)
